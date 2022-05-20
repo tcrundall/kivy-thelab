447ce4e5cd14fe7114c33fbbbcee5d113d052cd9
Timothy Crundall <tim.crundall@gmail.com>
2022-05-20 21:02:09 +0200

    Finish Layout section

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from kivy.app import App
 from kivy.metrics import dp
 from kivy.uix.anchorlayout import AnchorLayout
 from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.gridlayout import GridLayout
+# from kivy.uix.gridlayout import GridLayout
 from kivy.uix.stacklayout import StackLayout
 from kivy.uix.button import Button
 from kivy.uix.widget import Widget
